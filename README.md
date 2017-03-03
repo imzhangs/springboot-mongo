5e943525fe9863819920322f03b14ec74aea8be7
zhangshuai <shit_qq@163.com>
2017-03-03 13:43:32 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Spring boot MongoDB demo\n##spring boot 1.5.1 & mongo-3.4.2\n###take it   easy 
---
+++ src/main/java/com/springboot/mongo/SpringBootMongoAppMain.java
@@ -0,0 +1,14 @@
+package com.springboot.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoAppMain  {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoAppMain.class, args);
+	}
+
+}
